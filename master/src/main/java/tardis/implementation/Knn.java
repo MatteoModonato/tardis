62386f41e6c5e500e6385b6b78aa312541758ddb
MatteoModonato <m.modonato1@campus.unimib.it>
2020-06-01 11:49:04 +0200

    Added case management of zero distance, knn code refactoring

--- master/src/main/java/tardis/implementation/Knn.java
+++ master/src/main/java/tardis/implementation/Knn.java
@@ -1,7 +1,6 @@
 package tardis.implementation;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collections;
 import java.util.Comparator;
@@ -15,6 +14,9 @@ public class Knn {
 		List<Result> resultList = new ArrayList<Result>();
 		int countLabel0 = 0;
 		int countLabel1 = 0;
+		int countDistance0 = 0;
+		int countDistance0Label0 = 0;
+		int countDistance0Label1 = 0;
 		double total = 0;
 
 		for (StructureLaberPair structure : bloomFilterList) {
@@ -49,19 +51,49 @@ public class Knn {
 		}
 		
 		for(int i=0; i<nearestKPointsDistance.length; i++){
-        	total = total + nearestKPointsDistance[i];
-        }
+			if (nearestKPointsDistance[i]==0) {
+				countDistance0 = countDistance0+1;
+			}
+			else {
+				if (nearestKPoints[i]==0)
+					countDistance0Label0 = countDistance0Label0+1;
+				else
+					countDistance0Label1 = countDistance0Label1+1;
+			}
+			total = total + nearestKPointsDistance[i];
+		}
+		
 		double average = total / nearestKPointsDistance.length;
 		
+		//Gestisco anche i casi in cui una o più distanze è zero: non
+		//esiste ancora nel training set evidenza per quella query.
+		if (countDistance0==3 || countDistance0==2) {
+			Object[] output = new Object[]{0, 3, 0.0};
+			return output;
+		}
+		else if (countDistance0==1) {
+			if (countDistance0Label0==countDistance0Label1) {
+				Object[] output = new Object[]{0, 3, average};
+				return output;
+			}
+			else if (countDistance0Label0==2) {
+				Object[] output = new Object[]{0, 2, average};
+				return output;
+			}
+			else if (countDistance0Label1==2) {
+				Object[] output = new Object[]{1, 2, average};
+				return output;
+			}		
+		}
+		
 		if (countLabel0>countLabel1) {
-			Object[] output = new Object[]{findPopular(nearestKPoints), countLabel0, average};
+			Object[] output = new Object[]{0, countLabel0, average};
 			return output;
 		}
 		else {
-			Object[] output = new Object[]{findPopular(nearestKPoints), countLabel1, average};
+			Object[] output = new Object[]{1, countLabel1, average};
 			return output;
 		}
-
 	}
 
 	static class Result {	
@@ -78,35 +110,6 @@ public class Knn {
 		public int compare(Result a, Result b) {
 			return a.distance > b.distance ? -1 : a.distance == b.distance ? 0 : 1;
 		}
-	}
-
-	public static int findPopular(int[] a) {
-
-		if (a == null || a.length == 0)
-			return 0;
-
-		Arrays.sort(a);
-
-		int previous = a[0];
-		int popular = a[0];
-		int count = 1;
-		int maxCount = 1;
-
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] == previous)
-				count++;
-			else {
-				if (count > maxCount) {
-					popular = a[i-1];
-					maxCount = count;
-				}
-				previous = a[i];
-				count = 1;
-			}
-		}
-
-		return count > maxCount ? a[a.length-1] : popular;
-
 	}
 }
 
